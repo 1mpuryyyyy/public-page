88d0a8b260168d4421e2a2e7567682bb77f65d92
1mpuryyyyy <andreev200624@gmail.com>
2024-11-16 18:03:03 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import StartPage from "./StartPage/StartPage.jsx";
 import VisitCard from "./VisitCard/VisitCard.jsx";
 
